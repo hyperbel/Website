81212eaf270afc76e4f37cf45ddbfddfcedb1593
datewat <thomaszimmer524@gmail.com>
2021-06-05 17:10:06 +0200

    counter added

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 function AlertTest(content) {
     alert(content);
-  }
+}
+
+var counter = 0;
+
+function IncreaseCounter(count) {
+    counter = counter + count;
+    document.getElementById("CounterDisplay").innerHTML = counter;
+}
