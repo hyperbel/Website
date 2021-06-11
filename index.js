f84327283fc041790bb5147a9159f28869c5c35f
datewat <thomaszimmer524@gmail.com>
2021-06-11 08:59:58 +0200

    asdf

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function AlertTest(content) {
     alert(content);
 }
 
-
+var counter = 0;
 function IncreaseCounter(count) {
     counter = counter + count;
     document.getElementById("CounterDisplay").innerHTML = counter;
